6d74fbb14d842d6e4d12a61925ac8fec306b5db2
Nicky <43733664+Hathoom@users.noreply.github.com>
2022-03-04 17:49:19 -0800

    Added prefabs
    Made more collision examples.  Delete trigger, coffeecube trigger, and hidden trigger.

--- Assets/Scripts/DeleteCube.cs
+++ Assets/Scripts/DeleteCube.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TriggerCube : MonoBehaviour
+public class DeleteCube : MonoBehaviour
 {
     public float damage = 10f;
 
