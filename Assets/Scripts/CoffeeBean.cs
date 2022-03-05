1982e225a741d2166fcc66d9e35d5b883d568c47
Arthur Timpe <timp7118@csumb.ad.csumb.edu>
2022-03-04 17:28:51 -0800

    Added coffee bean object

---
+++ Assets/Scripts/CoffeeBean.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeeBean : MonoBehaviour {
+    public GameObject player;
+    //public GameObject gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        // When the player collides with the coffee bean the bean adds to bean counter in the GameManager and then deletes itself
+        if (collision.collider == player)
+        {
+            //gameManager.GetComponent<GameManager>().coffeebeans++;
+            Destroy(gameObject);
+        }
+    }
+}
